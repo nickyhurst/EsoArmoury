8c8a682a8e3c9237bc071f8c407f5968c1abbb5b
Nicholas HUrst <nicky.hurst@gmail.com>
2019-08-20 12:43:06 -0500

    Initial Commit

---
+++ EsoArmoury/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  EsoArmoury
+//
+//  Created by walnut on 8/20/19.
+//  Copyright © 2019 walnut. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
